a0a5c990bd66969e663b7891b50b3435ac9ed0f6
Diego Soria Rios <20172PF.CC0095@passofundo.ifsul.local>
2019-11-18 10:53:19 -0300

    Aula 18-11-19

--- PW/PW-2019-2-Web/src/java/br/edu/ifsul/DAO/DAOGenerico.java
+++ PW/PW-2019-2-Web/src/java/br/edu/ifsul/DAO/DAOGenerico.java
@@ -3,6 +3,7 @@ package br.edu.ifsul.dao;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -113,6 +114,7 @@ public class DAOGenerico<TIPO> implements Serializable {
         em.merge(obj);
     }
     
+    @RolesAllowed("ADMINISTRADOR")
     public void remove(TIPO obj) throws Exception {
         obj = em.merge(obj);
         em.remove(obj);
